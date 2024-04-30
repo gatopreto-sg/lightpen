c40bd14c830ef326e92aac8a351e1b66d4578c1f
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-04-30 01:09:22 -0300

    feat: sidebar, note and new-note components basic styling

--- src/app/components/leftsidebar/leftsidebar.component.ts
+++ src/app/components/leftsidebar/leftsidebar.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { NoteComponent } from '../note/note.component';
+import { NotesService } from '../../services/notes/notes.service';
+import { CommonModule } from '@angular/common';
+import { NewNoteComponent } from '../new-note/new-note.component';
 
 @Component({
   selector: 'app-leftsidebar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    NoteComponent,
+    NewNoteComponent
+  ],
   templateUrl: './leftsidebar.component.html',
   styleUrl: './leftsidebar.component.css'
 })
 export class LeftsidebarComponent {
+  notesService: NotesService = inject(NotesService)
 
+  notes = this.notesService.get_notes()
 }
